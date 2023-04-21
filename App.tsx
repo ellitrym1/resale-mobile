bf7cbd19c978d532f04829d28461513ec31218c9
Aditya Ratna Shakya <ellitrym1@gmail.com>
2023-04-21 13:40:23 +0100

    Test working

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
+import {IAd} from './types/Ad';
 
 const App = () => {
+  const [ads, setAds] = useState<[IAd]>();
+
+  useEffect(() => {
+    fetch(`http://10.0.2.2:5000/api/ads`, {method: 'GET'})
+      .then(res => res.json())
+      .then(data => {
+        setAds(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <View
       style={{
@@ -9,7 +21,7 @@ const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello, world!</Text>
+      <Text>{ads ? ads[0].ad_id : 'no data'}</Text>
     </View>
   );
 };
---
+++ types/Ad.tsx
@@ -0,0 +1,11 @@
+export interface IAd {
+  ad_id: string;
+  ad_title: string;
+  ad_description: string;
+  ad_category: number;
+  ad_images_url: string;
+  ad_status: string;
+  asking_price: string;
+  user_id: number;
+  created_at: string;
+}
